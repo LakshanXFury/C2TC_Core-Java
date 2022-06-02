83ba28e72eb955ff919e90ac4bab30e5b8d11ce1
Dell <Dell@Itachi>
2022-06-02 15:11:40 +0530

    6th commit

---
+++ oops/src/OOP/Demo.java
@@ -0,0 +1,13 @@
+package OOP;
+
+class Demo_class{
+	int x;
+}
+
+public class Demo {
+	public static void main(String[] args) {
+		Demo_class obj = new Demo_class();
+		obj.x = 700;
+		System.out.println(obj.x);
+	}
+}
