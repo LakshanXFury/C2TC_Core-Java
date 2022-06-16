fbf177cff6619ad97c5b239ba5b5f743ffd9d3e2
Dell <Dell@Itachi>
2022-06-16 14:45:06 +0530

    7th Commit

---
+++ Arrays/src/com/cg/arrays/Array.java
@@ -0,0 +1,12 @@
+package com.cg.arrays;
+
+public class Array {
+	public static void main(String[] args) {
+		int[] arr = {32,4,5,2,55};
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
+
